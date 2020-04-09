190c8f282642a76173af2a692e071eed02b37293
Michael Chun <60207632+mhkfchun@users.noreply.github.com>
2020-04-08 20:42:31 -1000

    Initial commit

---
+++ app/imports/startup/server/Mongo.js
@@ -0,0 +1,33 @@
+import { Meteor } from 'meteor/meteor';
+import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Contacts } from '../../api/contact/Contacts.js';
+
+/* eslint-disable no-console */
+
+/** Initialize the database with a default data document. */
+function addData(data) {
+  console.log(`  Adding: ${data.name} (${data.owner})`);
+  Stuffs.insert(data);
+}
+
+/** Initialize the collection if empty. */
+if (Stuffs.find().count() === 0) {
+  if (Meteor.settings.defaultData) {
+    console.log('Creating default data.');
+    Meteor.settings.defaultData.map(data => addData(data));
+  }
+}
+
+/** Initialize the database with a default data document. */
+function addContact(data) {
+  console.log(`  Adding: ${data.lastName} (${data.owner})`);
+  Contacts.insert(data);
+}
+
+/** Initialize the collection if empty. */
+if (Contacts.find().count() === 0) {
+  if (Meteor.settings.defaultContacts) {
+    console.log('Creating default contacts.');
+    Meteor.settings.defaultContacts.map(data => addContact(data));
+  }
+}
