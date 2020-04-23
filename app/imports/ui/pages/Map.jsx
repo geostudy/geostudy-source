184deea008ee6180330fc42e3d8e0921ba582aad
Newton Rasner <rasnern@hawaii.edu>
2020-04-22 19:53:52 -1000

    Added Google Maps into Maps page
    Added Google Maps code into Maps.
    
    Installed package for Google Maps API

--- app/imports/ui/pages/Map.jsx
+++ app/imports/ui/pages/Map.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import { Header, Image, Container } from 'semantic-ui-react';
+import { Loader } from 'semantic-ui-react';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
-/** A simple static component to render some text for the landing page. */
-class Map extends React.Component {
+/**  */
+class GoogleMaps extends React.Component {
+
   render() {
     return (
-        <div className='geostudy-map-back'>
-          <Header as='h1' inverted textAlign='center'>Map</Header>
-          <Container>
-            <Image src='https://i.imgur.com/YtapuCa.png'/>
-          </Container>
+        <div className="ui container" style={{ width: '70vw', height: '70vh', margin:'50px' }}>
+          <WrappedMap googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCeFS8T27RlCxMzV641cxQmBLh0nDo4fIA'}
+                      loadingElement={<div style={{ height: `100%` }}/>}
+                      containerElement={<div style={{ height: `100%` }}/>}
+                      mapElement={<div style={{ height: `100%` }}/>}
+          />
         </div>
     );
   }
 }
 
-export default Map;
+function Map() {
+  return <GoogleMap
+      defaultZoom={17}
+      defaultCenter={{lat:21.297274,lng:-157.817359} }
+  />;
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+export default GoogleMaps;
