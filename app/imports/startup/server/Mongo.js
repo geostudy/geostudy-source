e3b5645715ce2a2dfca34879d668a79e12c53c26
Michael Chun <60207632+mhkfchun@users.noreply.github.com>
2020-04-24 04:33:19 -1000

    Created Ratings Collection

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Spots } from '../../api/spot/Spots.js';
 import { Tags } from '../../api/tag/Tags.js';
+import { Ratings } from '../../api/rating/Ratings.js';
 
 /* eslint-disable no-console */
 
@@ -15,17 +16,31 @@ function addTags({ tag, spot }) {
   Tags.insert({ tag, spot });
 }
 
-/** Initialize the collection if empty. */
-if ((Meteor.settings.loadAssetsFile) && (Spots.find().count() === 0)) {
-  const assetsFileName = 'data.json';
-  console.log(`Loading spots from private/${assetsFileName}`);
-  const jsonData = JSON.parse(Assets.getText(assetsFileName));
-  jsonData.spots.map(spot => addSpots(spot));
+function addRatings({ rating, owner, spot }) {
+  console.log(`  Adding: ${spot} (${rating})`);
+  Tags.insert({ rating, owner, spot });
 }
 
-if ((Meteor.settings.loadAssetsFile) && (Tags.find().count() === 0)) {
-  const assetsFileName = 'data.json';
-  console.log(`Loading tags from private/${assetsFileName}`);
-  const jsonData = JSON.parse(Assets.getText(assetsFileName));
-  jsonData.tags.map(tag => addTags(tag));
+/** Initialize the collection if empty. */
+if (Meteor.settings.loadAssetsFile) {
+  if (Spots.find().count() === 0) {
+    const assetsFileName = 'data.json';
+    console.log(`Loading spots from private/${assetsFileName}`);
+    const jsonData = JSON.parse(Assets.getText(assetsFileName));
+    jsonData.spots.map(spot => addSpots(spot));
+  }
+
+  if (Tags.find().count() === 0) {
+    const assetsFileName = 'data.json';
+    console.log(`Loading tags from private/${assetsFileName}`);
+    const jsonData = JSON.parse(Assets.getText(assetsFileName));
+    jsonData.tags.map(tag => addTags(tag));
+  }
+
+  if (Ratings.find().count() === 0) {
+    const assetsFileName = 'data.json';
+    console.log(`Loading ratings from private/${assetsFileName}`);
+    const jsonData = JSON.parse(Assets.getText(assetsFileName));
+    jsonData.ratings.map(rating => addRatings(rating));
+  }
 }
--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Spots } from '../../api/spot/Spots';
 import { Tags } from '../../api/tag/Tags';
+import { Ratings } from '../../api/rating/Ratings';
 
 /** This subscription publishes only the documents associated with the logged in user */
 Meteor.publish('Spots', function publish() {
@@ -13,6 +14,13 @@ Meteor.publish('Spots', function publish() {
 Meteor.publish('Tags', function publish() {
   if (this.userId) {
     return Tags.find();
+  }
+  return this.ready();
+});
+
+Meteor.publish('Ratings', function publish() {
+  if (this.userId) {
+    return Ratings.find();
   }
   return this.ready();
 });
