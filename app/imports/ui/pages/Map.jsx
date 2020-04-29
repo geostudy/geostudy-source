b03f39dcf5be5c2eb4f9a884ebef54a308c845c1
Newton Rasner <rasnern@hawaii.edu>
2020-04-28 22:56:09 -1000

    1st attempt at markers

--- app/imports/ui/pages/Map.jsx
+++ app/imports/ui/pages/Map.jsx
@@ -1,18 +1,43 @@
 import React from 'react';
 import { Loader } from 'semantic-ui-react';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import { withScriptjs, withGoogleMap, GoogleMap } from 'react-google-maps';
+
+const Marker = ({ text }) => (
+    <div style={{
+      color: 'white',
+      background: 'blue',
+      padding: '1em 1em',
+      display: 'inline-flex',
+      textAlign: 'center',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius: '50%',
+      transform: 'translate(-50%, -50%)'
+    }}>
+      {text}
+    </div>
+);
 
 /**  */
 class GoogleMaps extends React.Component {
 
   render() {
     return (
-        <div className="ui container" style={{ width: '70vw', height: '70vh', margin:'50px' }}>
-          <WrappedMap googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo'}
-                      loadingElement={<div style={{ height: `100%` }}/>}
-                      containerElement={<div style={{ height: `100%` }}/>}
-                      mapElement={<div style={{ height: `100%` }}/>}
-          />
+        <div className="landing-background" style={{padding: '2em', margin: '-1em'}}>
+          <div className="ui container" style={{ width: '70vw', height: '70vh', margin: '' }}>
+            <WrappedMap
+                googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo'}
+                loadingElement={<div style={{ height: `100%` }}/>}
+                containerElement={<div style={{ height: `100%` }}/>}
+                mapElement={<div style={{ height: `100%` }}/>}
+            >
+              <Marker
+                  lat={21.298872}
+                  lng={-157.817204}
+                  text={'Test'}
+              />
+            </WrappedMap>
+          </div>
         </div>
     );
   }
@@ -21,7 +46,7 @@ class GoogleMaps extends React.Component {
 function Map() {
   return <GoogleMap
       defaultZoom={17}
-      defaultCenter={{lat:21.297274,lng:-157.817359} }
+      defaultCenter={{ lat: 21.297274, lng: -157.817359 }}
   />;
 }
 
