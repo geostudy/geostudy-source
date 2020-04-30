e704cf3916c91d9666fdf1df9de2f314688e0e7e
Newton Rasner <rasnern@hawaii.edu>
2020-04-29 20:13:54 -1000

    Update ListSpots.jsx

--- app/imports/ui/pages/ListSpots.jsx
+++ app/imports/ui/pages/ListSpots.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Item, Header, Loader, Container } from 'semantic-ui-react';
+import { Icon, Item, Header, Loader, Container, Divider, Pagination } from 'semantic-ui-react';
 import Spot from '/imports/ui/components/Spot';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
@@ -9,6 +9,17 @@ import { Ratings } from '../../api/rating/Ratings';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class ListSpots extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      activePage: 1,
+    };
+  }
+
+  handleChange = (e, data) => {
+    this.setState({ activePage: data.activePage });
+  };
 
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
@@ -20,30 +31,54 @@ class ListSpots extends React.Component {
     return (
         <Container>
           <Header as="h2" textAlign="center" inverted>Spots</Header>
-            <Item.Group>
-              {this.props.spots.map((spot, index) => <Spot key={index} Spots={Spots} spot={spot}
-               Ratings={Ratings} rating={this.props.ratings}/>)}
-            </Item.Group>
+          <Item.Group>
+            {this.props.spots.slice((this.state.activePage - 1) * 5, this.state.activePage * 5).map((spot, index) =>
+                <Spot key={index} Spots={Spots} spot={spot}
+                      Ratings={Ratings} rating={this.props.ratings}/>)}
+          </Item.Group>
+          <Divider/>
+          <Container textAlign={'center'}>
+            <Pagination
+                defaultActivePage={1}
+                ellipsisItem={{ content: <Icon name='ellipsis horizontal'/>, icon: true }}
+                firstItem={{ content: <Icon name='angle double left'/>, icon: true }}
+                lastItem={{ content: <Icon name='angle double right'/>, icon: true }}
+                prevItem={{ content: <Icon name='angle left'/>, icon: true }}
+                nextItem={{ content: <Icon name='angle right'/>, icon: true }}
+                totalPages={Math.ceil(this.props.spots
+                    .filter(spots).length / 20)}
+                onPageChange={this.handleChange}
+            />
+          </Container>
         </Container>
     );
   }
 }
 
 /** Require an array of Stuff documents in the props. */
-ListSpots.propTypes = {
+ListSpots
+    .propTypes = {
   spots: PropTypes.array.isRequired,
   ratings: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
 
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
-export default withTracker(() => {
-  // Get access to Stuff documents.
-  const subscription = Meteor.subscribe('Spots');
-  const subscription2 = Meteor.subscribe('Ratings');
-  return {
-    spots: Spots.find({}).fetch(),
-    ratings: Ratings.find({}).fetch(),
-    ready: (subscription.ready() && subscription2.ready()),
-  };
-})(ListSpots);
+export default withTracker(
+    () => {
+      // Get access to Stuff documents.
+      const
+          subscription = Meteor.subscribe('Spots');
+      const
+          subscription2 = Meteor.subscribe('Ratings');
+      return {
+        spots: Spots.find
+        ({}
+        ).fetch(),
+        ratings:
+            Ratings.find({}).fetch(),
+        ready:
+            (subscription.ready() && subscription2.ready()),
+      };
+    })
+(ListSpots);
