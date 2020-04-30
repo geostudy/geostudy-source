8ef97dce53f944d938b1e0e12ecdb4ec09687748
Newton Rasner <rasnern@hawaii.edu>
2020-04-29 20:41:34 -1000

    Update App.jsx

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import 'semantic-ui-css/semantic.css';
