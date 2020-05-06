58a43a51b16cc999709911d4d76e648f0416b0c1
Michael Chun <60207632+mhkfchun@users.noreply.github.com>
2020-05-06 10:08:01 -1000

    Update AddSpots.jsx

--- app/imports/ui/pages/AddSpots.jsx
+++ app/imports/ui/pages/AddSpots.jsx
@@ -3,10 +3,13 @@ import { Meteor } from 'meteor/meteor';
 import { Grid, Segment, Header } from 'semantic-ui-react';
 import { AutoForm, ErrorsField, LongTextField, SubmitField, TextField } from 'uniforms-semantic';
 import 'uniforms-bridge-simple-schema-2'; // required for Uniforms
+import { _ } from 'meteor/underscore';
+import PropTypes from 'prop-types';
 import swal from 'sweetalert';
 import SimpleSchema from 'simpl-schema';
-import { GoogleMap, withGoogleMap, withScriptjs } from 'react-google-maps';
+import { withTracker } from 'meteor/react-meteor-data';
 import { Spots } from '../../api/spot/Spots';
+import { Tags } from '../../api/tag/Tags';
 
 /** Create a schema to specify the structure of the data to appear in the form. */
 const formSchema = new SimpleSchema({
@@ -22,16 +25,19 @@ class AddSpots extends React.Component {
   /** On submit, insert the data. */
   submit(data, formRef) {
     const { name, image, location, description } = data;
+    const fieldTags = ['Air Conditioned', 'Indoors', 'Outdoors'];
+    const tagIds = _.map(fieldTags, (tagNames) => ());
+    console.log(tagIds);
     const owner = Meteor.user().username;
-    Spots.insert({ image, name, location, description, owner },
-        (error) => {
-          if (error) {
-            swal('Error', error.message, 'error');
-          } else {
-            swal('Success', 'Item added successfully', 'success');
-            formRef.reset();
-          }
-        });
+    // Spots.insert({ image, name, location, description, owner },
+    //     (error) => {
+    //       if (error) {
+    //         swal('Error', error.message, 'error');
+    //       } else {
+    //         swal('Success', 'Item added successfully', 'success');
+    //         formRef.reset();
+    //       }
+    //     });
   }
 
   /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
@@ -43,7 +49,7 @@ class AddSpots extends React.Component {
             <Header as="h2" textAlign="center" inverted>Add Study Spot</Header>
           </Grid.Row>
           <Grid.Row>
-            <Grid.Column width={8}>
+            <Grid.Column width={14}>
               <AutoForm ref={ref => {
                 fRef = ref;
               }} schema={formSchema} onSubmit={data => this.submit(data, fRef)}>
@@ -53,21 +59,13 @@ class AddSpots extends React.Component {
                   <TextField name='image' label='Picture of Study Spot'/>
                   <TextField name='location' label='Location/Address of Study Spot'/>
                   <LongTextField name='description' label='Describe the Study Spot'/>
+                  {console.log(this.props.tags)}
                   <SubmitField value='Submit'/>
                   <ErrorsField/>
                 </Segment>
               </AutoForm>
             </Grid.Column>
-            <Grid.Column width={8}>
-              <div className="ui container" style={{ width: '70vw', height: '46vh', margin: '0em' }}>
-                <WrappedMap
-                    googleMapURL={'https://maps.googleapis.com/maps/' +
-                    'api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo'}
-                    loadingElement={<div style={{ height: '100%' }}/>}
-                    containerElement={<div style={{ height: '100%' }}/>}
-                    mapElement={<div style={{ height: '100%' }}/>}
-                />
-              </div>
+            <Grid.Column width={14}>
             </Grid.Column>
           </Grid.Row>
         </Grid>
@@ -75,13 +73,20 @@ class AddSpots extends React.Component {
   }
 }
 
-function Map() {
-  return <GoogleMap
-      defaultZoom={17}
-      defaultCenter={{ lat: 21.297274, lng: -157.817359 }}
-  />;
-}
+AddSpots
+    .propTypes = {
+  Tags: PropTypes.object.isRequired,
+  tags: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
+};
 
-const WrappedMap = withScriptjs(withGoogleMap(Map));
-
-export default AddSpots;
+export default withTracker(
+    () => {
+      // Get access to Stuff documents.
+      const subscription = Meteor.subscribe('Tags');
+      return {
+        tags: Tags.find({}).fetch(),
+        ready: (subscription.ready()),
+      };
+    },
+)(AddSpots);
